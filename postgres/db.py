88950fa17d6593c2a84770c76f77c06564867be6
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-21 21:16:49 +0300

    feat: setup database, create tables

--- postgres/db.py
+++ postgres/db.py
@@ -1,10 +1,11 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
+
+import asyncio
 
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/helsinkibikes"
-
 async_engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
+
+
---
+++ postgres/setup_db.py
@@ -0,0 +1,17 @@
+from postgres.db import async_engine, Base
+from postgres import models
+import asyncio
+
+
+async def create_all_tables():
+    try:
+        async with async_engine.begin() as conn:
+            print(models.Base.metadata.tables)
+            print("Creating tables...")
+            await conn.run_sync(models.Base.metadata.create_all)
+            print("Tables created successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    asyncio.run(create_all_tables())
