da9b1ada0d12ff1cd55cea21b5596b89ed08f727
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-22 10:18:20 +0300

    feat: adjust models, add not null

--- postgres/models.py
+++ postgres/models.py
@@ -1,34 +1,34 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float
-from postgres.db import Base
+from sqlalchemy import Column, ForeignKey, SmallInteger, String, Float
+from postgres.database import Base
 #  SQLAlchemy ORM Models
 
 
 class Station(Base):
     __tablename__ = "stations"
 
-    station_id = Column(Integer, primary_key=True)
-    station_name = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    station_id = Column(SmallInteger, primary_key=True, nullable=False)
+    station_name = Column(String(50), nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
 
 
 class Trip(Base):
     __tablename__ = "trips"
 
-    trip_id = Column(Integer, primary_key=True)
-    departure_station_id = Column(Integer, ForeignKey("stations.station_id"))
-    return_station_id = Column(Integer, ForeignKey("stations.station_id"))
-    departure_year = Column(Integer)
-    departure_month = Column(Integer)
-    departure_day = Column(Integer)
-    departure_hour = Column(Integer)
-    departure_min = Column(Integer)
-    return_year = Column(Integer)
-    return_month = Column(Integer)
-    return_day = Column(Integer)
-    return_hour = Column(Integer)
-    return_min = Column(Integer)
-    distance_m = Column(Integer)
-    duration_min = Column(Integer)
+    trip_id = Column(SmallInteger, primary_key=True, nullable=False)
+    departure_station_id = Column(SmallInteger, ForeignKey("stations.station_id"), nullable=False)
+    return_station_id = Column(SmallInteger, ForeignKey("stations.station_id"), nullable=False)
+    departure_year = Column(SmallInteger)
+    departure_month = Column(String(9))
+    departure_day = Column(SmallInteger)
+    departure_hour = Column(SmallInteger)
+    departure_min = Column(SmallInteger)
+    return_year = Column(SmallInteger)
+    return_month = Column(String(9))
+    return_day = Column(SmallInteger)
+    return_hour = Column(SmallInteger)
+    return_min = Column(SmallInteger)
+    distance_m = Column(SmallInteger)
+    duration_min = Column(SmallInteger)
     speed_avg_km_h = Column(Float)
     air_temperature_deg_c = Column(Float)
