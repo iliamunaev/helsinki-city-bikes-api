66ca586cf091f779688deb7a716317e286f3c44c
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-22 17:15:28 +0300

    feat: add app, start_up_app, setup_pg_pool

---
+++ src/app/app.py
@@ -0,0 +1,29 @@
+import asyncpg
+from fastapi import FastAPI
+from .settings import AppSettings, PostgresSettings
+from .stations.routers import router as stations_router
+
+
+async def setup_pg_pool(app: FastAPI) -> None:
+    pg_settings = PostgresSettings()
+    pool = await asyncpg.create_pool(dsn=pg_settings.url)
+    app.state.pool = pool
+
+
+def start_up_app(settings: AppSettings) -> FastAPI:
+    app = FastAPI(
+        title=settings.TITLE,
+        version=settings.VERSION,
+    )
+
+    @app.on_event('startup')
+    async def startup() -> None:
+        await setup_pg_pool(app)
+
+    @app.on_event('shutdown')
+    async def shutdown() -> None:
+        await app.state.pool.close()
+
+    app.include_router(stations_router)
+
+    return app
