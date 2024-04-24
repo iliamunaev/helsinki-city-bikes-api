a1c75e89f546fbf5c7360fe9e3e8903f4efd0060
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-24 10:17:18 +0300

    fix: change @validator with @field_validator in schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,21 +1,22 @@
-from pydantic import BaseModel
-from pydantic.v1 import validator
+from pydantic import BaseModel, field_validator
+
 
 # Pydantic models for the API
+
 
 class Station(BaseModel):
     station_id: int
     station_name: str
-    latitude: int
-    longitude: int
+    latitude: float
+    longitude: float
 
-    @validator('latitude')
+    @field_validator('latitude')
     def check_latitude(cls, v):
         if not -90 <= v <= 90:
             raise ValueError("Latitude must be between -90 and 90.")
         return v
 
-    @validator('longitude')
+    @field_validator('longitude')
     def check_longitude(cls, v):
         if not -180 <= v <= 180:
             raise ValueError("Longitude must be between -180 and 180.")
