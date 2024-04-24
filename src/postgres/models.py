66fe304a96f42e45b4aa97196e3462fec0042663
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-24 14:10:20 +0300

    feat: assign Base directly in schemas

--- src/postgres/models.py
+++ src/postgres/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, ForeignKey, SmallInteger, String, Float
-
-from src.postgres.database import Base
-
+from sqlalchemy.orm import declarative_base
 
 #  SQLAlchemy ORM Models
+
+Base = declarative_base()
 
 
 class Station(Base):
