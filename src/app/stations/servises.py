ca23361ac10031ec5bffdaf9f0e55c759d71bad2
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-24 14:16:01 +0300

    fix: assign async to get_station_by_id

--- src/app/stations/servises.py
+++ src/app/stations/servises.py
@@ -7,5 +7,5 @@ class StationsService:
     def __init__(self, pool: asyncpg.Pool) -> None:
         self.stations_repo = StationsRepository(pool=pool)
 
-    def get_station_by_id(self, station_id: int):
-        return self.stations_repo.get_station_by_id(station_id=station_id)
+    async def get_station_by_id(self, station_id: int):
+        return await self.stations_repo.get_station_by_id(station_id=station_id)
