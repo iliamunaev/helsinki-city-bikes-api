a914ecfb57ba50c254508d880f33ff0a9663c431
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-24 14:14:39 +0300

    refactor: change get_station_by_id structure

--- src/app/stations/routers.py
+++ src/app/stations/routers.py
@@ -1,6 +1,8 @@
+from typing import List
+
 from fastapi import HTTPException, Depends, APIRouter
 
-from src.schemas import Station
+from src.app.schemas import Station
 from src.app.stations.servises import StationsService
 from src.depends import get_stations_service
 
@@ -13,10 +15,12 @@ def index():
 
 
 @router.get("/stations/{station_id}", response_model=Station)
-async def get_station_by_id(station_id: int,
-                            get_stations_service: StationsService = Depends(get_stations_service),
-                            ) -> Station:
-    station = await get_stations_service.get_station_by_id(station_id=station_id)
+async def get_station_by_id(
+        station_id: int,
+        get_stations_service: StationsService = Depends(get_stations_service)) -> Station:
+
+    station = await get_stations_service.get_station_by_id(station_id)
+
     if not station:
         raise HTTPException(status_code=404, detail="Station not found")
     return station
