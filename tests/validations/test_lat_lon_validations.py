43710b2b4ef3e4c0b735a288900b7cfafab25c0d
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-24 14:27:28 +0300

    test: add test_latitude_validation, test_longitude_validation

--- tests/validations/test_lat_lon_validations.py
+++ tests/validations/test_lat_lon_validations.py
@@ -1,40 +1,39 @@
 import pytest
 from pydantic import ValidationError
 
-from src.postgres.models import Station
+from src.app.schemas import Station
 
 
 @pytest.mark.parametrize(
     "latitude, expected_result",
     [
-        (-90, True),
-        (0, True),
-        (90, True),
-        (-91, ValueError),
-        (91, ValueError)
+        (-90.0, -90.0),
+        (0.0, 0.0),
+        (90.0, 90.0),
+        (-91.0, ValueError),
+        (91.0, ValueError)
     ]
 )
 def test_latitude_validation(latitude, expected_result):
-    if expected_result:
+    if isinstance(expected_result, float):
         station = Station(station_id=1, station_name="Central", latitude=latitude, longitude=0.0)
         assert station.latitude == latitude
     else:
         with pytest.raises(ValidationError):
             Station(station_id=1, station_name="Central", latitude=latitude, longitude=0.0)
 
-
 @pytest.mark.parametrize(
     "longitude, expected_result",
     [
-        (-180, True),
-        (0, True),
-        (180, True),
-        (-181, ValueError),
-        (181, ValueError)
+        (-180.0, -180.0),
+        (0.0, 0.0),
+        (180.0, 180.0),
+        (-181.0, ValueError),
+        (181.0, ValueError)
     ]
 )
 def test_longitude_validation(longitude, expected_result):
-    if expected_result:
+    if isinstance(expected_result, float):
         station = Station(station_id=1, station_name="Central", latitude=0.0, longitude=longitude)
         assert station.longitude == longitude
     else:
