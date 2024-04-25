0c35063aa73406ae1d7daecb89b4e2303413b492
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-25 10:38:04 +0300

    refactor: rename routers.py to router.py

--- src/app/stations/router.py
+++ src/app/stations/router.py
@@ -9,19 +9,14 @@ from src.depends import get_stations_service
 router = APIRouter()
 
 
-@router.get("/")
-def index():
-    return {"Hello": "World"}
-
-
-@router.get("/stations_names", response_model=List[StationName])
+@router.get("/names", response_model=List[StationName])
 async def get_stations_names(
         get_stations_service: StationsService = Depends(get_stations_service)) -> List[StationName]:
     stations_names = await get_stations_service.get_stations_names()
     return stations_names
 
 
-@router.get("/stations/{station_id}", response_model=Station)
+@router.get("/{station_id}", response_model=Station)
 async def get_station_by_id(
         station_id: int,
         get_stations_service: StationsService = Depends(get_stations_service)) -> Station:
