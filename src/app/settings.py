37d9ac7202f97003de1306126b8f402c9b0f7bfe
Ilia Munaev <ilyamunaev@gmail.com>
2024-04-27 11:51:17 +0300

    style: reformat settings.py with Black

--- src/app/settings.py
+++ src/app/settings.py
@@ -13,7 +13,7 @@ class PostgresSettings(BaseSettings):
 
     @property
     def url(self) -> str:
-        return f'postgres://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}'
+        return f"postgres://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}"
 
     class Config:
         case_sensitive = False
@@ -24,18 +24,18 @@ class AppSettings(BaseSettings):
     PORT: int = 8080
     IS_DEBUG: bool = True
 
-    TITLE: str = 'Helsinki City Bikes API'
-    VERSION: str = '0.1.0'
+    TITLE: str = "Helsinki City Bikes API"
+    VERSION: str = "0.1.0"
 
     class Config:
         case_sensitive = False
 
 
 class TestPostgresSettings(BaseSettings):
-    PG_HOST: str = 'localhost'
-    PG_USER: str = 'postgres'
-    PG_PASSWORD: str = 'postgres'
-    PG_DATABASE: str = 'helsinkibikes'
+    PG_HOST: str = "localhost"
+    PG_USER: str = "postgres"
+    PG_PASSWORD: str = "postgres"
+    PG_DATABASE: str = "helsinkibikes"
     PG_PORT: int = 5437
 
     class Config:
