f9827f27e6de46c66f8fe434a1020ef85977df98
Ilia Munaev <ilyamunaev@gmail.com>
2024-05-10 16:44:49 +0300

    refactor

--- db/repository.py
+++ db/repository.py
@@ -1,4 +1,3 @@
-
 import asyncpg
 
 
@@ -8,12 +7,10 @@ class StationsRepository:
 
     async def get_station_by_id(self, station_id: int) -> dict | None:
         record = await self.pool.fetchrow(
-            "SELECT * FROM stations WHERE station_id = $1",
-            station_id
+            "SELECT * FROM stations WHERE station_id = $1", station_id
         )
         return dict(record) if record else None
 
     async def get_stations_names(self):
         records = await self.pool.fetch("SELECT station_name FROM stations")
         return [dict(record) for record in records]
-
